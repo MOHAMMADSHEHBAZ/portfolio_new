71bbddb4d82df61b3750920dd0b5b510c0ec0723
InternBabu <shhbzsddq1@gmail.com>
2024-07-26 18:11:47 +0530

    Update Education2.jsx

--- src/components/Education2.jsx
+++ src/components/Education2.jsx
@@ -50,7 +50,7 @@ const Education2 = () => {
                     <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
                 </svg>
             </div>
-            <div className="hidden sm:flex w-full md:flex bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+            <div className="sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
         </div>
         <div className="mt-3 sm:pe-8">
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Senior Secondary School</h3>
